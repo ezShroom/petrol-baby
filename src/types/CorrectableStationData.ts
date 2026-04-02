861c1fe958a0de2c7989797508daff7a8d749880
ezShroom <68300730+ezShroom@users.noreply.github.com>
2026-04-03 00:29:07 +0100

    complete(?) correctable types and correction.md

--- src/types/CorrectableStationData.ts
+++ src/types/CorrectableStationData.ts
@@ -1,19 +1,65 @@
 import { z } from 'zod'
-import { ClosureType } from './ClosureType'
+
+export const InputCorrectableStationDataSchema = z.object({
+	nodeId: z.hex(),
+	tradingName: z.string(),
+	brandName: z.string(),
+	phone: z.string().nullable(),
+	isMotorwayServiceStation: z.boolean(),
+	isSupermarketServiceStation: z.boolean(),
+	address: z.object({
+		address1: z.string().nullable(),
+		address2: z.string().nullable(),
+		city: z.string().nullable(),
+		county: z.string().nullable(),
+		country: z.string().nullable(),
+		postcode: z.string().nullable()
+	}),
+	coords: z.object({
+		latitude: z.number(),
+		longitude: z.number()
+	})
+})
+export type InputCorrectableStationData = z.infer<
+	typeof InputCorrectableStationDataSchema
+>
 
 export const OutputCorrectableStationDataSchema = z.object({
-	nodeId: z
-		.string()
-		.describe('Fuel Finder node ID associated with this station'),
+	nodeId: z.hex().describe('Fuel Finder node ID associated with this station'),
 	tradingName: z
 		.string()
 		.describe(
-			"A human-friendly name to identify this specific station, potentially including the brand name and place - for example, 'Asda Petrol East Retford' or 'JET Thamesmead Service Station'."
+			"A human-friendly name to identify this specific station, potentially including the place - for example, 'Asda Petrol East Retford' or 'JET Thamesmead Service Station'. Make this recognisable."
 		),
 	brandName: z
 		.string()
 		.describe(
-			"A human-friendly **brand name only** - for example, 'Asda' or 'JET'. Follow the guidel"
+			"A human-friendly **brand name only** - for example, 'Asda' or 'JET'."
 		),
-	closure: z.enum(ClosureType).nullable()
+	phone: z.string().optional(),
+	address: z.object({
+		address1: z.string().describe('Address Line 1'),
+		address2: z.string().optional().describe('Address line 2'),
+		city: z.string().optional(),
+		country: z.union([
+			z.literal('England'),
+			z.literal('Wales'),
+			z.literal('Scotland'),
+			z.literal('Northern Ireland')
+		]),
+		postcode: z
+			.string()
+			.describe('Full postcode with a space between the outcode and incode')
+	}),
+	coords: z.object({
+		latitude: z.number(),
+		longitude: z.number()
+	}),
+	potentialDuplicates: z
+		.array(z.hex())
+		.optional()
+		.describe('Array of node IDs of potential duplicate stations')
 })
+export type OutputCorrectableStationData = z.infer<
+	typeof OutputCorrectableStationDataSchema
+>
