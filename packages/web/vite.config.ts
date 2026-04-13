656581a905fbbea3f1cc7fc3788f4a6d08577872
ezShroom <68300730+ezShroom@users.noreply.github.com>
2026-04-13 19:51:50 +0100

    prettier

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,10 @@
 /// <reference types="bun-types" />
 
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()], server: { allowedHosts: process.env.NODE_ENV === 'development' || undefined } });
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+	server: { allowedHosts: process.env.NODE_ENV === 'development' || undefined }
+})
